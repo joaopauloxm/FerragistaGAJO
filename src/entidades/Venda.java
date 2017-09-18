57e4f287da4b2b2e633ee5ca3af9725e8b5c7bd6
joaopauloxm <joaopauloxm18@outlook.com>
2017-09-18 20:48:34 -0300

    Venda
    Implementação

--- src/entidades/Venda.java
+++ src/entidades/Venda.java
@@ -1,5 +1,41 @@
 package entidades;
 
+import java.util.Date;
+
 public class Venda {
     private int codigo;
+    private Date dataHora;
+    private double valorTotal;
+
+    public Venda(int codigo, Date dataHora, double valorTotal) {
+        this.codigo = codigo;
+        this.dataHora = dataHora;
+        this.valorTotal = valorTotal;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public Date getDataHora() {
+        return dataHora;
+    }
+
+    public void setDataHora(Date dataHora) {
+        this.dataHora = dataHora;
+    }
+
+    public double getValorTotal() {
+        return valorTotal;
+    }
+
+    public void setValorTotal(double valorTotal) {
+        this.valorTotal = valorTotal;
+    }
+    
+    
 }
